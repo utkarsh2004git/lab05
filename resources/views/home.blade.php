37d807dae44a35dbcf76bd244ccf6977b1bfbfaf
ukarsh2004git <utkarshshahare2004@gmail.com>
2023-11-08 11:21:38 +0530

    Learned about Controllers

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Welcome page</h1>
-    <a href="home">Go to home</a>
+    <h1>This is Home Page</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\DemoControllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/",function(){
+    return view("welcome");
 });
+
+
+Route::get("/home",[DemoController::class,'index']);
